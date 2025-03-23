b8e7dfd7e2630a70492fe533b5964201886b8f2e
shomrkm <shochan1985@gmail.com>
2025-03-23 17:13:05 +0900

    feat: update the messages page.

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -1,5 +1,46 @@
-import { Text } from 'smarthr-ui'
+import { Base, BaseColumn, Heading, InformationPanel, ResponseMessage, Stack, Text } from 'smarthr-ui'
+import styled from 'styled-components'
 
 export const Messages = () => {
-  return <Text>メッセージの出し方</Text>
+  return (
+    <Stack gap={1.5}>
+      <Heading>よく使うメッセージの出し方</Heading>
+      <Stack gap={1.5}>
+        <Text weight="bold">BaseColumn + ResponseMessage</Text>
+        <StyledBase>
+          <Stack>
+            <BaseColumn>
+              <ResponseMessage type="info" >ここに情報を表示します。</ResponseMessage>
+            </BaseColumn>
+            <BaseColumn>
+              <ResponseMessage type="warning" >ここに警告を表示します。</ResponseMessage>
+            </BaseColumn>
+            <StyledBaseColumn>
+              <ResponseMessage type="warning" >横幅を固定した警告を表示します。</ResponseMessage>
+            </StyledBaseColumn>
+          </Stack>
+        </StyledBase>
+        <Text weight="bold">InformationPanel</Text>
+        <StyledBase>
+          <Stack>
+            <InformationPanel type="info" title="情報" >
+              ここに情報を表示します。
+            </InformationPanel>
+            <InformationPanel type="warning" title="警告" >
+              ここに警告を表示します。
+            </InformationPanel>
+          </Stack>
+        </StyledBase>
+      </Stack>
+    </Stack>
+  )
 }
+
+const StyledBase = styled(Base)`
+  padding: ${({ theme }) => theme.space(2)};
+  border-radius : ${({ theme }) => theme.radius.m};
+`
+
+const StyledBaseColumn = styled(BaseColumn)`
+  width: 400px;
+`
